d2d3d8cdaab7609b09a5d053891a71707a6ef0c8
verssae <sparky@hanyang.ac.kr>
2021-05-20 15:13:13 +0900

    add JUnit Life Cycle examples

--- src/utils/BinaryCalculatorTest.java
+++ src/utils/BinaryCalculatorTest.java
@@ -2,6 +2,10 @@ package utils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,12 +15,14 @@ class BinaryCalculatorTest {
 
 	@Test
 	void test() {
+		System.out.println("Test method call");
 		assertEquals(3/2, calc.div(3, 2));
 	}
 	
 	@Test
 	@DisplayName("Multiple Unit Test")
 	void assertTest() {
+		System.out.println("Assert Test method call");
 		assertAll("calc div", 
 			() -> assertEquals(3/2, calc.div(3, 2)),
 			() -> assertSame(3/2, calc.div(3, 2)),
@@ -26,7 +32,29 @@ class BinaryCalculatorTest {
 	
 	@Test 
 	void failureTest() {
+		System.out.println("Failure Test method call");
 		assertTrue(false);
 	}
+	
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("BeforeAll method call\n");
+	}
+	
+	@BeforeEach
+	void beforeEach() {
+		System.out.println("BeforeEach method call");
+	}
+	
+	@AfterEach
+	void afterEach() {
+		System.out.println("AfterEach method call");
+	}
+	
+	@AfterAll
+	static void afterAll() {
+		System.out.println("\nAfterAll method call");
+	}
+	
 
 }
